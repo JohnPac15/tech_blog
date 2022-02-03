6b6a83d3589893d9890742b47b49369d76f86dc1
JohnPac <johnpac15@yahoo.com>
2022-02-03 13:38:51 -0600

    app is now able to display post on loading the homepage

--- controllers/homepage-routes.js
+++ controllers/homepage-routes.js
@@ -1,8 +1,30 @@
 const router = require('express').Router()
 const sequelize = require('../config/connection')
+const { User, Post } = require('../models')
 
 router.get('/', (req, res) => {
-  res.render('homepage');
+  Post.findAll({
+    order: [['created_at', 'DESC']],
+    attributes: [
+      'id',
+      'title',
+      'created_at'
+    ],
+    include:[{
+        model: User,
+        attributes: ['username']
+    }]
+  })
+  .then(dbPostData => {
+    const post = dbPostData.map((post) => post.get({ plain: true }));
+    console.log({ post });
+
+    res.render('homepage', { post });
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router
